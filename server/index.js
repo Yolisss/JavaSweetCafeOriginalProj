939f82faa1f0461c699f73f55de1c3fb595778c2
Yolis <yolisisabel28@gmail.com>
2022-11-02 10:13:34 -0700

    server fetching api working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,41 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+
+// import iceCoffeeRoutes from "./routes/coffee.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = 2032;
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json("hello from techtonica");
+});
+
+// GET REQUEST FOR WOMENS CLOTHING
+app.get("/icecoffee", (req, res) => {
+  let URL = `https://api.sampleapis.com/coffee/iced`;
+  console.log("Ice Coffee was recieved");
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.get("/hotcoffee", (req, res) => {
+  let URL = `https://api.sampleapis.com/coffee/hot`;
+  console.log("Ice Coffee was recieved");
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+//listens for incoming request
 app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
